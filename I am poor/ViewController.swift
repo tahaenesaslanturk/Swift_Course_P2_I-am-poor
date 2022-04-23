d45624e2147527e11b5e348037af81543a44b0ef
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-04-23 16:19:55 +0300

    Initial Commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Taha Enes Aslantürk on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
